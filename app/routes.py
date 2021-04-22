41c7fa14ad0b6746c34a16df5802f978dad3b16d
simon <simon@adadevelopersacademy.org>
2021-04-21 17:34:57 -0700

    adds endpoint to get one book by id, GET /<book_id>

--- app/routes.py
+++ app/routes.py
@@ -26,3 +26,14 @@ def handle_books():
         db.session.commit()
 
         return make_response(f"Book {new_book.title} successfully created", 201)
+
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
