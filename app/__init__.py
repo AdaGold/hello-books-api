4e037483b1303026f8896e6cd708e71069d30a7a
simon <simon@adadevelopersacademy.org>
2021-04-21 17:00:31 -0700

    registers hello world bp, makes hello world endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,8 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # Register Blueprints here
+    from .routes import hello_world_bp
+    app.register_blueprint(hello_world_bp)
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world_bp", __name__)
+
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    return "Hello, World!"
