45046ed9bf0526201b7c45023410972ae00e1b6b
simon <simon@adadevelopersacademy.org>
2021-04-21 17:22:56 -0700

    defines book model, adds model config and migrations

---
+++ app/models/book.py
@@ -0,0 +1,7 @@
+from app import db
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
