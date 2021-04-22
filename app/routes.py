b131b1acdf8ae4362c20009cb85e4737536d2bf6
simon <simon@adadevelopersacademy.org>
2021-04-21 17:39:59 -0700

    adds update functionality for PUT /<book_id> requests

--- app/routes.py
+++ app/routes.py
@@ -28,12 +28,22 @@ def handle_books():
         return make_response(f"Book {new_book.title} successfully created", 201)
 
 
-@books_bp.route("/<book_id>", methods=["GET"])
+@books_bp.route("/<book_id>", methods=["GET", "PUT"])
 def handle_book(book_id):
     book = Book.query.get(book_id)
 
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-    }
+    if request.method == "GET":
+        return {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        book.title = form_data["title"]
+        book.description = form_data["description"]
+
+        db.session.commit()
+
+        return make_response(f"Book #{book.id} successfully updated")
