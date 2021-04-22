1e2c6020a650999d11c424b2b3eea991f35916bd
simon <simon@adadevelopersacademy.org>
2021-04-21 17:32:45 -0700

    handles getting a list of books with GET /books

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,28 @@
 from app import db
 from app.models.book import Book
-from flask import request, Blueprint, make_response
+from flask import request, Blueprint, make_response, jsonify
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 
-@books_bp.route("", methods=["POST"])
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
 
-    db.session.add(new_book)
-    db.session.commit()
+        db.session.add(new_book)
+        db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+        return make_response(f"Book {new_book.title} successfully created", 201)
