1525c8c5d96aba8e5911979cca26bb11eb2edfbf
simon <simon@adadevelopersacademy.org>
2021-04-21 17:13:50 -0700

    adds GET /hello/JSON endpoint

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,12 @@ hello_world_bp = Blueprint("hello_world_bp", __name__)
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
     return "Hello, World!"
+
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
