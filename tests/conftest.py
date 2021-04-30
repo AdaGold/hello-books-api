8072c1527c223a86d4c3e855ea5e2c61401194fb
simon <simon@adadevelopersacademy.org>
2021-04-29 19:58:02 -0700

    adds tests/, configures conftest with fixtures to setup app

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from app import create_app
+from app import db
+
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
