9fb887408311a571105e83e3aba2c23719f65bf4
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-14 14:12:58 -0700

    Add expire all

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,16 @@
 import pytest
 from app import create_app
 from app import db
+from flask.signals import request_finished
 
 
 @pytest.fixture
 def app():
     app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()    
 
     with app.app_context():
         db.create_all()
