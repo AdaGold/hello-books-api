93a9cb6332b3aab4e27603e8e5991159be33e6df
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-22 15:38:02 -0700

    Add book class and data

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+]
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
