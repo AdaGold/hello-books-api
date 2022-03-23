2c5c3bc9bd02aa3789f8e62506ba7d9eb2eb9c30
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-23 10:31:46 -0700

    Comment out harcoded book data

--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,17 @@
 from os import abort
 from flask import Blueprint, jsonify, abort, make_response
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-]
+# books = [
+#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# ]
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
