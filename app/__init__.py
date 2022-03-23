ac1689ff4d1d198f0bd7f73a093a27d07680b708
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-23 10:25:14 -0700

    Connect database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    # Import models here
+    from app.models.book import Book
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # Register Blueprints here
     from .routes import hello_world_bp
