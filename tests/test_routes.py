35c1b3685f41e615a804064d418d86e9f4ba8f03
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-23 16:33:06 -0700

    Test get one book complete

---
+++ tests/test_routes.py
@@ -0,0 +1,22 @@
+def test_get_all_books_with_no_records(client):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_one_book(client, two_saved_books):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+
