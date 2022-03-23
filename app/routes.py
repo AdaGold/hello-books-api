b6ac2eaa02d1ff70e467acc8a1045ee4e6efd282
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-23 10:27:13 -0700

    Remove hello world routes and comment out book routes

--- app/routes.py
+++ app/routes.py
@@ -17,67 +17,42 @@ books = [
 hello_world_bp = Blueprint("hello_world_bp", __name__)
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         abort(make_response({"message":f"book {book_id} invalid"}, 400))
 
-    found = False
-    for book in books:
-        if book.id == book_id:
-            found = True
+#     found = False
+#     for book in books:
+#         if book.id == book_id:
+#             found = True
 
-    if found is False:
-        abort(make_response({"message":f"book {book_id} not found"}, 404))
+#     if found is False:
+#         abort(make_response({"message":f"book {book_id} not found"}, 404))
         
 
-@books_bp.route("", methods=["GET"])
-def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-        )
-    return jsonify(books_response)
+# @books_bp.route("", methods=["GET"])
+# def handle_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append(
+#             {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             }
+#         )
+#     return jsonify(books_response)
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    validate_book(book_id)
+# @books_bp.route("/<book_id>", methods=["GET"])
+# def handle_book(book_id):
+#     validate_book(book_id)
 
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
-    
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    return "Hello, World!"
-
-
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    return {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-
-
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
+#     for book in books:
+#         if book.id == book_id:
+#             return {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description,
+#             }
