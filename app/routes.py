bb3c175cb65b22e8b2403e698fb84d7edb028233
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-23 10:52:01 -0700

    Delete endpoint

--- app/routes.py
+++ app/routes.py
@@ -66,7 +66,14 @@ def update_book(book_id):
 
     return make_response(f"Book #{book.id} successfully updated")
 
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
 
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")
 
 
 
