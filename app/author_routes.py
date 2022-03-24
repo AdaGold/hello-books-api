22d282bb66d02dd6f4df58c50a21afe58f8b85f5
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-24 14:23:50 -0700

    Create book and read book nested routes

--- app/author_routes.py
+++ app/author_routes.py
@@ -1,8 +1,22 @@
 from app import db
 from app.models.author import Author
+from app.models.book import Book
 from flask import Blueprint, jsonify, abort, make_response, request
 
 authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
+
+def validate_author(author_id):
+    try:
+        author_id = int(author_id)
+    except:
+        abort(make_response({"message":f"author {author_id} invalid"}, 400))
+
+    author = Author.query.get(author_id)
+
+    if not author:
+        abort(make_response({"message":f"author {author_id} not found"}, 404))
+
+    return author
 
 @authors_bp.route("", methods=["POST"])
 def create_author():
@@ -28,4 +42,34 @@ def read_all_authors():
         )
     return jsonify(authors_response)
 
+@authors_bp.route("/<author_id>/books", methods=["POST"])
+def create_book(author_id):
+
+    author = validate_author(author_id)
+
+    request_body = request.get_json()
+    new_book = Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        author=author
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
+@authors_bp.route("/<author_id>/books", methods=["GET"])
+def read_books(author_id):
+
+    author = validate_author(author_id)
+
+    books_response = []
+    for book in author.books:
+        books_response.append(
+            {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+            }
+        )
+    return jsonify(books_response)
 
