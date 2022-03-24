b55bade9908ca55159f44a0680657c9c2320ef52
Becca Elenzil <becca@adadevelopersacademy.org>
2022-03-24 14:12:59 -0700

    Add author_id and relationship columns

--- app/models/author.py
+++ app/models/author.py
@@ -3,3 +3,4 @@ from app import db
 class Author(db.Model):
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   name = db.Column(db.String)
+  books = db.relationship("Book", back_populates="author")
--- app/models/book.py
+++ app/models/book.py
@@ -4,3 +4,5 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    author = db.relationship("Author", back_populates="books")
