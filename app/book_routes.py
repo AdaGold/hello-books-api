b4c9d4904de32d85ad4f80ed939aa9132e3ea942
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-10-13 12:42:45 -0700

    Add content from Refactoring topic

--- app/book_routes.py
+++ app/book_routes.py
@@ -4,24 +4,23 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-def validate_book(book_id):
+def validate_model(cls, model_id):
     try:
-        book_id = int(book_id)
+        model_id = int(model_id)
     except:
-        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
 
-    book = Book.query.get(book_id)
+    model = cls.query.get(model_id)
 
-    if not book:
-        abort(make_response({"message":f"book {book_id} not found"}, 404))
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
 
-    return book
+    return model
 
 @books_bp.route("", methods=["POST"])
 def create_book():
     request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
+    new_book = Book.from_dict(request_body)
 
     db.session.add(new_book)
     db.session.commit()
@@ -39,27 +38,17 @@ def read_all_books():
 
     books_response = []
     for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-        )
+        books_response.append(book.to_dict())
     return jsonify(books_response)
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def read_one_book(book_id):
-    book = validate_book(book_id)
-    return {
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        }
+    book = validate_model(Book, book_id)
+    return book.to_dict()
 
 @books_bp.route("/<book_id>", methods=["PUT"])
 def update_book(book_id):
-    book = validate_book(book_id)
+    book = validate_model(Book, book_id)
 
     request_body = request.get_json()
 
@@ -72,7 +61,7 @@ def update_book(book_id):
 
 @books_bp.route("/<book_id>", methods=["DELETE"])
 def delete_book(book_id):
-    book = validate_book(book_id)
+    book = validate_model(Book, book_id)
 
     db.session.delete(book)
     db.session.commit()
---
+++ tests/test_models.py
@@ -0,0 +1,109 @@
+from app.models.book import Book
+import pytest
+
+def test_to_dict_no_missing_data():
+    # Arrange
+    test_data = Book(id = 1,
+                    title="Ocean Book",
+                    description="watr 4evr")
+
+    # Act
+    result = test_data.to_dict()
+
+    # Assert
+    assert len(result) == 3
+    assert result["id"] == 1
+    assert result["title"] == "Ocean Book"
+    assert result["description"] == "watr 4evr"
+
+def test_to_dict_missing_id():
+    # Arrange
+    test_data = Book(title="Ocean Book",
+                    description="watr 4evr")
+
+    # Act
+    result = test_data.to_dict()
+
+    # Assert
+    assert len(result) == 3
+    assert result["id"] is None
+    assert result["title"] == "Ocean Book"
+    assert result["description"] == "watr 4evr"
+
+def test_to_dict_missing_title():
+    # Arrange
+    test_data = Book(id=1,
+                    description="watr 4evr")
+
+    # Act
+    result = test_data.to_dict()
+
+    # Assert
+    assert len(result) == 3
+    assert result["id"] == 1
+    assert result["title"] is None
+    assert result["description"] == "watr 4evr"
+
+def test_to_dict_missing_description():
+    # Arrange
+    test_data = Book(id = 1,
+                    title="Ocean Book")
+
+    # Act
+    result = test_data.to_dict()
+
+    # Assert
+    assert len(result) == 3
+    assert result["id"] == 1
+    assert result["title"] == "Ocean Book"
+    assert result["description"] is None
+
+def test_from_dict_returns_book():
+    # Arrange
+    book_data = {
+        "title": "New Book",
+        "description": "The Best!"
+    }
+
+    # Act
+    new_book = Book.from_dict(book_data)
+
+    # Assert
+    assert new_book.title == "New Book"
+    assert new_book.description == "The Best!"
+
+def test_from_dict_with_no_title():
+    # Arrange
+    book_data = {
+        "description": "The Best!"
+    }
+
+    # Act & Assert
+    with pytest.raises(KeyError, match = 'title'):
+        new_book = Book.from_dict(book_data)
+
+def test_from_dict_with_no_description():
+    # Arrange
+    book_data = {
+        "title": "New Book"
+    }
+
+    # Act & Assert
+    with pytest.raises(KeyError, match = 'description'):
+        new_book = Book.from_dict(book_data)
+
+def test_from_dict_with_extra_keys():
+    # Arrange
+    book_data = {
+        "extra": "some stuff",
+        "title": "New Book",
+        "description": "The Best!",
+        "another": "last value"
+    }
+    
+    # Act
+    new_book = Book.from_dict(book_data)
+
+    # Assert
+    assert new_book.title == "New Book"
+    assert new_book.description == "The Best!"
