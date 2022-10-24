8afb7746ae254b62d9609c09b68efcf8ea9dbb0d
francescafr <hello@francesca.fyi>
2022-10-24 09:14:27 -0700

    added error handling to get book method

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 class Book:
     def __init__(self, id, title, description):
         self.id = id
@@ -25,14 +25,25 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods = ["GET"])
 def handle_book(book_id):
-    book_id = int(book_id)
+    book = validate_book(book_id)
+    return{
+        "id" : book.id, 
+        "title" : book.title, 
+        "description" : book.description
+    }
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+    
     for book in books:
         if book.id == book_id:
-                return{
-                    "id" : book.id, 
-                    "title" : book.title, 
-                    "description" : book.description
-                }
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
+
 
 
 hello_world_bp = Blueprint("hello_world", __name__, url_prefix="/hello-world")
