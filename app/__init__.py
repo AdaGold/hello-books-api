bf5659fb3bb35a7d0e0c59c8eb9a99fe9a4c52a7
Kyra Patton <kyra.patton@adadevelopersacademy.org>
2023-04-27 10:22:23 -0700

    Add former connection string as a comment

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,8 @@ def create_app(test_config=None):
 
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get( 
+        # "SQLALCHEMY_DATABASE_URI")
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
             "RENDER_DATABASE_URL")
     else:
