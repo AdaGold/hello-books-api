fe8d18a725e396c90b8e6ac7f23bd3eb078c5896
Kyra Patton <kyra.patton@adadevelopersacademy.org>
2023-04-27 10:20:57 -0700

    Change SQLALCHEMY_DATABASE_URI connection string

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app(test_config=None):
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+            "RENDER_DATABASE_URL")
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
