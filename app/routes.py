e0985b13b3b1dbab0d025809615d2e7c48fa4c06
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-04 11:05:39 -0800

    Add read one function with name that reflects the action taken

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,6 @@ books = [
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["GET"])
 @books_bp.get("/")
 def get_all_books():
     books_response = []
@@ -27,3 +26,14 @@ def get_all_books():
             }
         )
     return jsonify(books_response)
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
