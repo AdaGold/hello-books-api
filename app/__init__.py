1b3a5273255f2efcdf16e6291c4700a4691656ea
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-04 09:50:09 -0800

    Update registered blueprint in __init__ to match books blueprint created in the routes file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from .routes import hello_world_bp
+from .routes import books_bp
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     # Register Blueprints here
-    app.register_blueprint(hello_world_bp)
+    app.register_blueprint(books_bp)
 
     return app
