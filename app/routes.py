41f16f877bd156a3146b1ebcdeb72eb3868ffaf3
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-05 09:30:33 -0800

    Add post with clear name and sending back a JSON reply rather than string

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,25 @@
-from flask import Blueprint, jsonify, abort, make_response
-
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-# books = [
-#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-# ]
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.book import Book
+from .db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+@books_bp.post("/")
+def create_book():
+    request_body = request.get_json()
+    title = request_body.get("title")
+    description = request_body.get("description")
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+    return make_response(response, 201)
 
 # @books_bp.get("/")
 # def get_all_books():
