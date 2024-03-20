63b705ef1d01a2ae042526c0226c9d54b9dc4c01
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 14:13:10 -0700

    Add Hello World routes and register them in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .routes import hello_world_bp
 
 def create_app():
     app = Flask(__name__)
+
+    # Register Blueprints here
+    app.register_blueprint(hello_world_bp)
 
     return app
