e92fca7e3da3122f87c418834f6964a64d4723e4
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 15:35:06 -0700

    Adds tests folder and fixtures in conftest.py we'll use for testing later

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from app import create_app
+from app.db import db
+from flask.signals import request_finished
+
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
