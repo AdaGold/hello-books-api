6233fdfa5d83d28e8c5aedf12e881409f7fe28c5
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 15:00:44 -0700

    Adds create book route

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,6 +1,25 @@
-from flask import Blueprint, abort, make_response
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from .db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json()
+    title = request_body.get("title")
+    description = request_body.get("description")
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+    return make_response(response, 201)
 
 # @books_bp.get("")
 # def get_all_books():
