e21afc191aff2d9634dffaa62280c51ea78cca06
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 15:42:43 -0700

    Add test fixture to store 2 book in the datbase. Add test for get_one_book endpoint.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app.db import db
 from flask.signals import request_finished
+from app.models.book import Book
 from dotenv import load_dotenv
 import os
 
@@ -30,3 +31,18 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def two_saved_books(app):
+    # Arrange
+    ocean_book = Book(title="Ocean Book",
+                      description="watr 4evr")
+    mountain_book = Book(title="Mountain Book",
+                         description="i luv 2 climb rocks")
+
+    db.session.add_all([ocean_book, mountain_book])
+    # Alternatively, we could do
+    # db.session.add(ocean_book)
+    # db.session.add(mountain_book)
+    db.session.commit()
