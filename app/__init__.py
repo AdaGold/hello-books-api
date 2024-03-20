9050886ed3c861132123774dfcd3b676b4e2a834
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 15:28:03 -0700

    Update create_app to use a test database in testing mode

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,19 @@ from flask import Flask
 from .db import db, migrate
 from .models import book
 from .routes.book_routes import books_bp
+import os
 
 def create_app():
     app = Flask(__name__)
 
+    if not test_config:
+        database_uri = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    else:
+        app.config["TESTING"] = True
+        database_uri = os.environ.get('SQLALCHEMY_TEST_DATABASE_URI')
+    
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 
     db.init_app(app)
     migrate.init_app(app, db)
