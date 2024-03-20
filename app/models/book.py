de2d3cc3c1990f3fd18cb2882769160afe07d986
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 16:38:01 -0700

    Adds to_dict method to Book model. Updates routes to use to_dict. Increase test suite to cover changed routes and to_dict.

--- app/models/book.py
+++ app/models/book.py
@@ -5,3 +5,11 @@ class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+
+    def to_dict(self):
+        book_as_dict = {}
+        book_as_dict["id"] = self.id
+        book_as_dict["title"] = self.title
+        book_as_dict["description"] = self.description
+
+        return book_as_dict
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,6 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
+
 def test_get_all_books_with_no_records(client):
     # Act
     response = client.get("/books")
@@ -6,6 +9,50 @@ def test_get_all_books_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_all_books_with_two_records(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body[0] == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+    assert response_body[1] == {
+        "id": 2,
+        "title": "Mountain Book",
+        "description": "i luv 2 climb rocks"
+    }
+
+def test_get_all_books_with_title_query_matching_none(client, two_saved_books):
+    # Act
+    data = {'title': 'Desert Book'}
+    response = client.get("/books", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_all_books_with_title_query_matching_one(client, two_saved_books):
+    # Act
+    data = {'title': 'Ocean Book'}
+    response = client.get("/books", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body[0] == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
 
 def test_get_one_book_succeeds(client, two_saved_books):
     # Act
@@ -20,12 +67,67 @@ def test_get_one_book_succeeds(client, two_saved_books):
         "description": "watr 4evr"
     }
 
+def test_get_one_book_missing_record(client, two_saved_books):
+    # Act
+    response = client.get("/books/3")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message":"book 3 not found"}
+
+def test_get_one_book_invalid_id(client, two_saved_books):
+    # Act
+    response = client.get("/books/cat")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {"message":"book cat invalid"}
+
 def test_create_one_book(client):
     # Act
     response = client.post("/books", json={
         "title": "New Book",
         "description": "The Best!"
     })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 1,
+        "title": "New Book",
+        "description": "The Best!"
+    }
+
+def test_create_one_book_no_title(client):
+    # Arrange
+    test_data = {"description": "The Best!"}
+
+    # Act & Assert
+    with pytest.raises(IntegrityError, match="'title': None"):
+        response = client.post("/books", json=test_data)
+
+def test_create_one_book_no_description(client):
+    # Arrange
+    test_data = {"title": "New Book"}
+
+    # Act & Assert
+    with pytest.raises(IntegrityError, match="'description': None"):
+        response = client.post("/books", json=test_data)
+
+def test_create_one_book_with_extra_keys(client):
+    # Arrange
+    test_data = {
+        "extra": "some stuff",
+        "title": "New Book",
+        "description": "The Best!",
+        "another": "last value"
+    }
+
+    # Act
+    response = client.post("/books", json=test_data)
     response_body = response.get_json()
 
     # Assert
