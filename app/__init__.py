6eeabb9569a85b1fb8ac7b5adbc256b9279a2064
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 14:56:09 -0700

    Adds book model, removes hardcoded data in routes.py.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from .db import db, migrate
+from .models import book
 from .routes.book_routes import books_bp
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # Register Blueprints here
     app.register_blueprint(books_bp)
--- app/models/book.py
+++ app/models/book.py
@@ -1,11 +1,7 @@
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-]
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
