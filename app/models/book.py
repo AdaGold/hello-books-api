0efb5e31550818dc3ee0d937ff1463d5d22af38b
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 14:56:09 -0700

    Adds book model, removes hardcoded data in routes.py.

---
+++ app/models/book.py
@@ -0,0 +1,7 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
