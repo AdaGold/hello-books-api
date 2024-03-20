87a8c6bb0800592da3be534b33e5b72d5104465b
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 14:23:13 -0700

    Replaces Hello World routes with Book class and route to get all books

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from .routes.hello_world_routes import hello_world_bp
+from .routes.book_routes import books_bp
 
 def create_app():
     app = Flask(__name__)
 
     # Register Blueprints here
-    app.register_blueprint(hello_world_bp)
+    app.register_blueprint(books_bp)
 
     return app
