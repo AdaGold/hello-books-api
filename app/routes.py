458e9736cef958102f8ff78f4d86d3c7aa5b0a67
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 14:44:40 -0700

    Add error handling for missing or invalid book ids when fetching a single book

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -29,11 +29,24 @@ def get_all_books():
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
-    book_id = int(book_id)
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response , 400))
+
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
+            return book
+
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))
