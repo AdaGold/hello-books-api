5e4d25383d9327dc0a07735be1bde9f353b940d5
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 14:13:10 -0700

    Add Hello World routes and register them in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,10 @@
 from flask import Flask
-from .db import db, migrate
-from .models import book
-from .routes.book_routes import books_bp
+from .routes.hello_world_routes import hello_world_bp
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-
+    
     # Register Blueprints here
-    app.register_blueprint(books_bp)
-
+    app.register_blueprint(hello_world_bp)
+    
     return app
