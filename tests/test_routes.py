2b845629e7acc04d776882d9c35a9da881a2c28c
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 16:48:31 -0700

    Add from_dict class method to Book model. Update routes to use from_dict. Add testing for affected code still missing tests and new from_dict function.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,3 @@
-import pytest
-from sqlalchemy.exc import IntegrityError
-
 def test_get_all_books_with_no_records(client):
     # Act
     response = client.get("/books")
@@ -105,17 +102,25 @@ def test_create_one_book_no_title(client):
     # Arrange
     test_data = {"description": "The Best!"}
 
-    # Act & Assert
-    with pytest.raises(IntegrityError, match="'title': None"):
-        response = client.post("/books", json=test_data)
+    # Act
+    response = client.post("/books", json=test_data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {'message': 'Invalid request: missing title'}
 
 def test_create_one_book_no_description(client):
     # Arrange
     test_data = {"title": "New Book"}
+    
+    # Act
+    response = client.post("/books", json=test_data)
+    response_body = response.get_json()
 
-    # Act & Assert
-    with pytest.raises(IntegrityError, match="'description': None"):
-        response = client.post("/books", json=test_data)
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {'message': 'Invalid request: missing description'}
 
 def test_create_one_book_with_extra_keys(client):
     # Arrange
