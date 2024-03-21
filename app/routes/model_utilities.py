63de5cb644a44db4e2b6a12735e4c04c8c13eab5
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-20 17:12:32 -0700

    Reorganizing to benefit future work. Renames source and test files specific to books to include book in the title. Refactor validate_book to a more generic function validate_model and move it to a new file model_utilities.py. Add testing for validate_model.

---
+++ app/routes/model_utilities.py
@@ -0,0 +1,18 @@
+from flask import abort, make_response
+from ..db import db
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        response = {"message": f"{cls.__name__} {model_id} invalid"}
+        abort(make_response(response , 400))
+
+    query = db.select(cls).where(cls.id == model_id)
+    model = db.session.scalar(query)
+    
+    if not model:
+        response = {"message": f"{cls.__name__} {model_id} not found"}
+        abort(make_response(response, 404))
+    
+    return model
