9070a28a29cf6d39bcbfee2009913c23c8c733b5
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-22 10:17:58 -0700

    Adds nested routes to create a book with an author and get all books from an author. Adss testing for new routes and changes to Book model's to_dict and from_dict functions.

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, make_response, abort
 from app.models.author import Author
+from app.models.book import Book
 from .route_utilities import validate_model
 from ..db import db
 
@@ -24,12 +25,31 @@ def create_author():
 @bp.get("")
 def get_all_authors():
     query = db.select(Author)
-
+    
     name_param = request.args.get("name")
     if name_param:
         query = query.where(Author.name.ilike(f"%{name_param}%"))
-
+    
     authors = db.session.scalars(query.order_by(Author.id))
     authors_response = [author.to_dict() for author in authors]
 
     return authors_response
+
+@bp.post("/<author_id>/books")
+def create_book_with_author(author_id):
+    author = validate_model(Author, author_id)
+    
+    request_body = request.get_json()
+    request_body["author_id"] = author.id
+    new_book = Book.from_dict(request_body)
+    
+    db.session.add(new_book)
+    db.session.commit()
+    
+    return make_response(new_book.to_dict(), 201)
+
+@bp.get("/<author_id>/books")
+def get_books_by_author(author_id):
+    author = validate_model(Author, author_id)
+    response = [book.to_dict() for book in author.books]
+    return response
