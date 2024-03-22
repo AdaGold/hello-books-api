0f82680555ef8e28f29dba4d3d90a8fa7ccda7cb
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-22 10:17:58 -0700

    Adds nested routes to create a book with an author and get all books from an author. Adss testing for new routes and changes to Book model's to_dict and from_dict functions.

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -33,20 +33,15 @@ def get_all_authors():
     authors = db.session.scalars(query.order_by(Author.id))
     authors_response = [author.to_dict() for author in authors]
 
-    return authors_response
+return authors_response
 
 @bp.post("/<author_id>/books")
 def create_book_with_author(author_id):
     author = validate_model(Author, author_id)
-    
+
     request_body = request.get_json()
     request_body["author_id"] = author.id
-    new_book = Book.from_dict(request_body)
-
-    db.session.add(new_book)
-    db.session.commit()
-    
-    return make_response(new_book.to_dict(), 201)
+    return create_model(Book, request_body)
 
 @bp.get("/<author_id>/books")
 def get_books_by_author(author_id):
