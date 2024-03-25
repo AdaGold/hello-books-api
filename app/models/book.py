9269ca831d54366541fc0d6b7a4ab7c6466d1bfd
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-25 13:02:06 -0700

    Minor fix to spacing in Books that changed during rebase

--- app/models/book.py
+++ app/models/book.py
@@ -5,7 +5,7 @@ class Book:
         self.description = description
 
 books = [
-         Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-         Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-         Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-         ]
+    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+]
