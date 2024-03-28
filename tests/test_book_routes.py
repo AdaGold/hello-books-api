314f17096d1ae3348c7ad584f01c2d7ca3a38891
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-28 09:22:14 -0700

    Add first test to test_book_routes.py

---
+++ tests/test_book_routes.py
@@ -0,0 +1,8 @@
+def test_get_all_books_with_no_records(client):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
