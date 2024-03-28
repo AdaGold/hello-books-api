559939b901ae228ac28fc1ae7c125412ad8a0d8e
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-28 15:32:04 -0700

    Move books PUT route back to direct key access

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -25,8 +25,8 @@ def update_book(book_id):
     book = validate_model(Book, book_id)
     request_body = request.get_json()
 
-    book.title = request_body.get("title")
-    book.description = request_body.get("description")
+    book.title = request_body["title"]
+    book.description = request_body["description"]
     db.session.commit()
 
     return Response(status=204, mimetype="application/json") # 204 No Content
