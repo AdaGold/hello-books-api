c3208470c29f09d42601f427df4c9907e012a23e
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-28 11:58:04 -0700

    Undo changes to refactor model creation and filtering to model_utilities - these will be done in a later branch

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -1,15 +1,34 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response, abort
 from app.models.author import Author
-from .model_utilities import create_model, get_models_with_filters
+from ..db import db
 
 bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
 
 @bp.post("")
 def create_author():
     request_body = request.get_json()
-    return create_model(Author, request_body)
+
+    try:
+        new_author = Author.from_dict(request_body)
+        
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+    
+    db.session.add(new_author)
+    db.session.commit()
+
+    return make_response(new_author.to_dict(), 201)
 
 @bp.get("")
 def get_all_authors():
-    filters = request.args
-    return get_models_with_filters(Author, filters)
+    query = db.select(Author)
+
+    name_param = request.args.get("name")
+    if name_param:
+        query = query.where(Author.name.ilike(f"%{name_param}%"))
+
+    authors = db.session.scalars(query.order_by(Author.id))
+    authors_response = [author.to_dict() for author in authors]
+
+    return authors_response
--- tests/test_model_utilities.py
+++ tests/test_model_utilities.py
@@ -1,7 +1,6 @@
-from app.routes.model_utilities import validate_model, create_model, get_models_with_filters
-from werkzeug.exceptions import HTTPException
+from app.routes.model_utilities import validate_model
 from app.models.book import Book
-from app.models.author import Author
+from werkzeug.exceptions import HTTPException
 import pytest
 
 def test_validate_model(two_saved_books):
@@ -26,64 +25,3 @@ def test_validate_model_invalid_id(two_saved_books):
     # cause an `HTTPException` when an `abort` statement is reached 
     with pytest.raises(HTTPException):
         result_book = validate_model(Book, "cat")
-
-# We use the `client` fixture because we need an   
-# application context to work with the database session
-def test_create_model_book(client):
-    # Arrange
-    test_data = {
-        "title": "New Book",
-        "description": "The Best!"
-    }
-
-    # Act
-    result = create_model(Book, test_data)
-
-    # Assert
-    assert result.status_code == 201
-    assert result.get_json() == {
-        "id": 1,
-        "title": "New Book",
-        "description": "The Best!"
-    }
-
-def test_create_model_book_missing_data(client):
-    # Arrange
-    test_data = {
-        "description": "The Best!"
-    }
-
-    # Act & Assert
-    # Calling `create_model` without being invoked by a route will
-    # cause an `HTTPException` when an `abort` statement is reached 
-    with pytest.raises(HTTPException):
-        result_book = create_model(Book, test_data) 
-
-# We use the `client` fixture because we need an   
-# application context to work with the database session
-def test_create_model_author(client):
-    # Arrange
-    test_data = {
-        "name": "New Author"
-    }
-
-    # Act
-    result = create_model(Author, test_data)
-
-    # Assert
-    assert result.status_code == 201
-    assert result.get_json() == {
-        "id": 1,
-        "name": "New Author"
-    }
-
-def test_get_models_with_filters_one_matching_book(two_saved_books):
-    # Act
-    result = get_models_with_filters(Book, {"title": "ocean"})
-
-    # Assert
-    assert result == [{
-        "id": 1,
-        "title": "Ocean Book",
-        "description": "watr 4evr"
-    }]
