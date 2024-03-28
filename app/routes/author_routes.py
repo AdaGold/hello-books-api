24ab76f6ba09f58d17126b3c19338d6adc0362fb
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-28 12:51:20 -0700

    Refactor model creation and fetching and filtering models to the model_utilities file

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -1,44 +1,24 @@
-from flask import Blueprint, request, make_response, abort
+from flask import Blueprint, request
 from app.models.author import Author
 from app.models.book import Book
-from .route_utilities import validate_model
-from ..db import db
+from .model_utilities import create_model, get_models_with_filters, validate_model
 
 bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
 
 @bp.post("")
 def create_author():
     request_body = request.get_json()
-
-    try:
-        new_author = Author.from_dict(request_body)
-        
-    except KeyError as error:
-        response = {"message": f"Invalid request: missing {error.args[0]}"}
-        abort(make_response(response, 400))
-    
-    db.session.add(new_author)
-    db.session.commit()
-
-    return make_response(new_author.to_dict(), 201)
+    return create_model(Author, request_body)
 
 @bp.get("")
 def get_all_authors():
-    query = db.select(Author)
-    
-    name_param = request.args.get("name")
-    if name_param:
-        query = query.where(Author.name.ilike(f"%{name_param}%"))
-    
-    authors = db.session.scalars(query.order_by(Author.id))
-    authors_response = [author.to_dict() for author in authors]
-
-return authors_response
+    filters = request.args
+    return get_models_with_filters(Author, filters)
 
 @bp.post("/<author_id>/books")
 def create_book_with_author(author_id):
     author = validate_model(Author, author_id)
-
+    
     request_body = request.get_json()
     request_body["author_id"] = author.id
     return create_model(Book, request_body)
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, abort, make_response, request, Response
+from flask import Blueprint, request, Response
 from app.models.book import Book
-from .model_utilities import validate_model
+from .model_utilities import validate_model, create_model, get_models_with_filters
 from ..db import db
 
 bp = Blueprint("books_bp", __name__, url_prefix="/books")
@@ -8,60 +8,16 @@ bp = Blueprint("books_bp", __name__, url_prefix="/books")
 @bp.post("")
 def create_book():
     request_body = request.get_json()
-
-    try:
-        new_book = Book.from_dict(request_body)
-
-    except KeyError as error:
-        response = {"message": f"Invalid request: missing {error.args[0]}"}
-        abort(make_response(response, 400))
-    
-    db.session.add(new_book)
-    db.session.commit()
-
-    return make_response(new_book.to_dict(), 201)
+    return create_model(Book, request_body)
 
 @bp.get("")
 def get_all_books():
-    # create a basic select query without any filtering
-    query = db.select(Book)
-
-    # If we have a `title` query parameter, we can add on to the query object
-    title_param = request.args.get("title")
-    if title_param:
-        # Match the title_param exactly, including capitalization
-        # query = query.where(Book.title == title_param)
-
-        # If we want to allow partial matches, we can use the % wildcard with `like()`
-        # If `title_param` contains "Great", the code below will match 
-        # both "The Great Gatsby" and "Great Expectations"
-        # query = query.where(Book.title.like(f"%{title_param}%"))
-
-        # If we want to allow searching case-insensitively, 
-        # we can use ilike instead of like
-        query = query.where(Book.title.ilike(f"%{title_param}%"))
-
-    # If we have other query parameters, we can continue adding to the query. 
-    # As we did above, we must reassign the `query`` variable to capture the new clause we are adding. 
-    # If we don't reassign `query``, we are calling the `where` function but are not saving the resulting filter
-    description_param = request.args.get("description")
-    if description_param:
-        # In case there are books with similar titles, we can also filter by description
-        query = query.where(Book.description.ilike(f"%{description_param}%"))
-
-    books = db.session.scalars(query.order_by(Book.id))
-    # We could also write the line above as:
-    # books = db.session.execute(query).scalars()
-
-    books_response = []
-    for book in books:
-        books_response.append(book.to_dict())
-    return books_response
+    filters = request.args
+    return get_models_with_filters(Book, filters)
 
 @bp.get("/<book_id>")
 def get_one_book(book_id):
     book = validate_model(Book, book_id)
-
     return book.to_dict()
 
 @bp.put("/<book_id>")
