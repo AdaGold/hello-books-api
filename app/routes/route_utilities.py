d6b744ace6cf5580ddd084c30853ff25695f0674
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-04-02 14:52:54 -0700

    Remove changes introduced in rebase so only requirements change shows in PR

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -7,7 +7,7 @@ def validate_model(cls, model_id):
     except:
         response = {"message": f"{cls.__name__} {model_id} invalid"}
         abort(make_response(response , 400))
-    
+
     query = db.select(cls).where(cls.id == model_id)
     model = db.session.scalar(query)
     
@@ -20,7 +20,7 @@ def validate_model(cls, model_id):
 def create_model(cls, model_data):
     try:
         new_model = cls.from_dict(model_data)
-    
+
     except KeyError as error:
         response = {"message": f"Invalid request: missing {error.args[0]}"}
         abort(make_response(response, 400))
@@ -28,7 +28,7 @@ def create_model(cls, model_data):
     db.session.add(new_model)
     db.session.commit()
 
-return make_response(new_model.to_dict(), 201)
+    return make_response(new_model.to_dict(), 201)
 
 def get_models_with_filters(cls, filters=None):
     query = db.select(cls)
@@ -39,5 +39,6 @@ def get_models_with_filters(cls, filters=None):
                 query = query.where(getattr(cls, attribute).ilike(f"%{value}%"))
 
     models = db.session.scalars(query.order_by(cls.id))
+
     models_response = [model.to_dict() for model in models]
     return models_response
