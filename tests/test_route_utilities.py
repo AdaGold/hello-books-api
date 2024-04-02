e062d277d42b1e9fe88eb23947758f32d606f721
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-04-02 13:11:38 -0700

    Remove unneccessary fixture from route utilities test

--- tests/test_route_utilities.py
+++ tests/test_route_utilities.py
@@ -12,7 +12,7 @@ def test_validate_model(two_saved_books):
     assert result_book.title == "Ocean Book"
     assert result_book.description == "watr 4evr"
 
-def test_validate_model_missing_record(client, two_saved_books):
+def test_validate_model_missing_record(two_saved_books):
     # Act & Assert
     # Calling `validate_model` without being invoked by a route will
     # cause an `HTTPException` when an `abort` statement is reached 
