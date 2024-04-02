c37953e19ad863b3183803744ddbcd07ae1889ea
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-04-02 11:57:21 -0700

    Re-add tests from prior tile accidentally removed in refactor

--- tests/test_book_routes.py
+++ tests/test_book_routes.py
@@ -1,5 +1,27 @@
 import pytest
 
+def test_get_all_books_with_no_records(client):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_one_book_succeeds(client, two_saved_books):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+
 def test_create_one_book(client):
     # Act
     response = client.post("/books", json={
