987b78a8ad04f14eb2ad77fb40b1897c3d40641c
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-04-03 16:16:44 -0700

    remove added blank line from rebase to keep PR to just affected files

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -39,6 +39,5 @@ def get_models_with_filters(cls, filters=None):
                 query = query.where(getattr(cls, attribute).ilike(f"%{value}%"))
 
     models = db.session.scalars(query.order_by(cls.id))
-
     models_response = [model.to_dict() for model in models]
     return models_response
