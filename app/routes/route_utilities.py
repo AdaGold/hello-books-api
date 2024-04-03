9e8ae013883506db0aef2b41f3921468aa2e3d83
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-04-03 16:26:04 -0700

    match spacing changed in route_utilities during rebase so it doesn't show in PR

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -7,7 +7,7 @@ def validate_model(cls, model_id):
     except:
         response = {"message": f"{cls.__name__} {model_id} invalid"}
         abort(make_response(response , 400))
-    
+
     query = db.select(cls).where(cls.id == model_id)
     model = db.session.scalar(query)
     
@@ -20,11 +20,11 @@ def validate_model(cls, model_id):
 def create_model(cls, model_data):
     try:
         new_model = cls.from_dict(model_data)
-    
+        
     except KeyError as error:
         response = {"message": f"Invalid request: missing {error.args[0]}"}
         abort(make_response(response, 400))
-
+    
     db.session.add(new_model)
     db.session.commit()
 
