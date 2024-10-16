87ef129b7ea4efabf1e72c776276564c83eafbc6
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-10-16 09:40:16 -0700

    Remove code that will be introduced in the next lesson with to_dict refactor

--- app/models/book.py
+++ app/models/book.py
@@ -5,14 +5,6 @@ class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
-
-    def to_dict(self):
-        book_as_dict = {}
-        book_as_dict["id"] = self.id
-        book_as_dict["title"] = self.title
-        book_as_dict["description"] = self.description
-
-        return book_as_dict
     
     @classmethod
     def from_dict(cls, book_data):
